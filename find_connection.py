dfe866269ca0956c6fa930de652e95eb68b8f6ae
Hayden <beastflow@gmail.com>
2024-05-27 12:37:07 +0200

    fix formatting issues for parameter presentation

--- find_connection.py
+++ find_connection.py
@@ -2,6 +2,6 @@ from brute_forcer.brute_force2 import Bruteforce
 from pprint import pprint
 
 parameters = Bruteforce.find_connection_params()
-print("\n* * * * * Connection parameters found * * * * *")
+print("\n* * * * * Connection parameters found * * * * *\n")
 pprint(parameters)
 print("\n* * * * * * * * * *\n")
--- find_parameter_addresses.py
+++ find_parameter_addresses.py
@@ -2,5 +2,6 @@ from brute_forcer.brute_force2 import Bruteforce
 from pprint import pprint
 
 parameters = Bruteforce.find_parameter_addresses()
-
+print("\n* * * * * Connection parameters found * * * * *\n")
 pprint(parameters)
+print("\n* * * * * * * * * *\n")
